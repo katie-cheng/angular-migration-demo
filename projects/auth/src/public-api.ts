f940f91505041a15b243dcf7d8af6de9fb713c1a
Marta Kowalczyk <marta.kowalczyk@northwind-retail.example>
2023-12-22 14:40:59 +0000

    auth: library scaffold

---
+++ projects/auth/src/public-api.ts
@@ -0,0 +1,14 @@
+/*
+ * Public API Surface of auth
+ */
+
+export * from './lib/auth.config';
+export * from './lib/auth.models';
+export * from './lib/auth.service';
+export * from './lib/auth.guard';
+export * from './lib/auth.interceptor';
+export * from './lib/auth.module';
+export * from './lib/correlation';
+export * from './lib/mfa.service';
+export * from './lib/session-initializer';
+export * from './lib/session-store.service';
