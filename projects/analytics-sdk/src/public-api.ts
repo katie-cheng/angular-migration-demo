c8ec30db977322a5096454048ea2b2920f002537
Hugo Baptiste <hugo.baptiste@northwind-retail.example>
2024-02-14 15:42:58 +0000

    analytics-sdk: library scaffold

---
+++ projects/analytics-sdk/src/public-api.ts
@@ -0,0 +1,10 @@
+/*
+ * Public API Surface of analytics-sdk
+ */
+
+export * from './lib/analytics.config';
+export * from './lib/analytics.models';
+export * from './lib/analytics.service';
+export * from './lib/analytics-sdk.module';
+export * from './lib/consent.service';
+export * from './lib/route-analytics.service';
