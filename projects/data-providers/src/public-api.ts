832bf71dc6c5cb8e32c99182c2e21c02faeb6230
Tom Ellery <tom.ellery@northwind-retail.example>
2024-03-25 12:30:27 +0000

    data-providers: library scaffold

---
+++ projects/data-providers/src/public-api.ts
@@ -0,0 +1,10 @@
+/*
+ * Public API Surface of data-providers
+ */
+
+export * from './lib/banking.models';
+export * from './lib/banking.facade';
+export * from './lib/data-providers.module';
+export * from './lib/ledger.provider';
+export * from './lib/legacy-core.provider';
+export * from './lib/provider.tokens';
